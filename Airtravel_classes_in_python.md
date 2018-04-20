15ee97520530f02a895d4750d79c46cd0325c7a4
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-04-20 17:32:29 +0200

    python-first: classes in Python. Collaborating classes

--- Airtravel_classes_in_python.md
+++ Airtravel_classes_in_python.md
@@ -4,6 +4,8 @@ Classes in Python: airtavel (flight, aircraft...)
 
 ## Usage
 
+### Two classes
+
 >>> from airtravel import *
 >>> a = Aircraft("G-EUPT", "Airbus A319", num_rows=22, num_seats_per_row=6)
 >>> a.registration()
@@ -12,3 +14,10 @@ Classes in Python: airtavel (flight, aircraft...)
 'Airbus A319'
 >>> a.seating_plan()
 (range(1, 23), 'ABCDEF')
+
+## Collaborating classes
+
+>>> from airtravel import *
+>>> f = Flight("BA758", Aircraft("G-EUPT", "Airbus A319", num_rows=22, num_seats_per_row=6))
+>>> f.aircraft_model()
+'Airbus A319'
--- airtravel.py
+++ airtravel.py
@@ -1,22 +1,27 @@
 class Flight:
+    """A flight with a particular passenger aircraft."""
 
-    def __init__(self, number):
+    def __init__(self, number, aircraft):
         if not number[:2].isalpha():
             raise ValueError("No airline code in '{}'".format(number))
 
         if not number[:2].isupper():
             raise ValueError("Invalid airline code '{}".format(number))
 
-        if not (number[2:].isdigit() and int(number[1:]) <= 9999):
+        if not (number[2:].isdigit() and int(number[2:]) <= 9999):
             raise ValueError("Invalid route number '{}".format(number))
 
         self._number = number
+        self._aircraft = aircraft
 
     def number(self):
         return self._number
 
     def airline(self):
         return self._number[:2]
+
+    def aircraft_model(self):
+        return self._aircraft.model()
 
 
 class Aircraft:
